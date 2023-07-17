cd268be27599a6c590f00c1ad5960f1d88419e2f
DonMatano <mwakimamatano@gmail.com>
2023-07-17 06:41:16 +0300

    Push changes

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,52 @@
-use leptos::*;
+use leptos::{ev::InputEvent, *};
 fn main() {
     mount_to_body(|cx| view! { cx, <App/> })
 }
 
+enum InputType {
+    Rem,
+    Px,
+}
+
 #[component]
 fn App(cx: Scope) -> impl IntoView {
-    let (count, set_count) = create_signal(cx, 0);
-    let doubleCount = move || count() * 2;
+    let (px_value, set_px_value) = create_signal(cx, 16.0);
+    let rem_value = px_value() / 16.0;
+    fn verify_value(value: &str) -> f32 {
+        value.parse::<f32>().expect("Invalid value")
+    }
+
+    let update_rem_value = move |e: InputEvent| {
+        let res = verify_value(&event_target_value(&e)) * 16.0;
+        set_px_value(res);
+    };
+
+    let handle_px_input = move |e: InputEvent| {
+        let res = verify_value(&event_target_value(&e));
+        set_px_value(res);
+    };
+
     view! { cx,
-        <button
-            class:red=move || count() % 2 == 1
-            on:click=move |_| {
-            set_count.update(|n| *n += 1)
-        }
-        >"Click me: " {count}</button>
-        <ProgressBar progress=count max=10 />
-        <ProgressBar progress=Signal::derive(cx, doubleCount) max=20 />
+        <CalculatedInput on:input=update_rem_value calculated_value=rem_value  set_value=Signal::derive(cx, set_rem_value(px_value)) label="Rem" input_type=InputType::Rem />
+        <CalculatedInput calculated_value=px_value set_value=set_px_value label="Px" input_type=InputType::Px />
     }
 }
 
-/// Show progress to a goal
 #[component]
-fn ProgressBar(
+fn CalculatedInput<F>(
     cx: Scope,
-    /// The maximum value of the progress bar.
-    #[prop(default = 100)]
-    max: u16,
-    /// How much progress should be shown.
-    #[prop(into)]
-    progress: Signal<i32>,
-) -> impl IntoView {
-    view! { cx,
-        <progress
-            max=max
-            value=progress
-        />
+    #[prop(into)] calculated_value: Signal<f32>,
+    label: &'static str,
+    on_value_changed: F,
+) -> impl IntoView
+where
+    F: Fn(InputEvent) + 'static,
+{
+    view! {cx,
+        <label>{label}</label>
 
+        <input type="number"
+        on:input=on_value_changed
+        placeholder={label} value={calculated_value} props:value={calculated_value} />
     }
 }
