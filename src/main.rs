7d0c6557a54f4f09cf88bd8e22734d9950923417
DonMatano <mwakimamatano@gmail.com>
2023-07-17 11:01:20 +0300

    Fix input not updating

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,63 @@
-use leptos::{ev::InputEvent, *};
+use leptos::{ev::Event, html::Input, *};
 fn main() {
     mount_to_body(|cx| view! { cx, <App/> })
-}
-
-enum InputType {
-    Rem,
-    Px,
 }
 
 #[component]
 fn App(cx: Scope) -> impl IntoView {
     let (px_value, set_px_value) = create_signal(cx, 16.0);
-    let rem_value = px_value() / 16.0;
+    let rem_value = move || px_value() / 16.0;
+    let px_input_ref = create_node_ref::<html::Input>(cx);
+    let rem_input_ref = create_node_ref::<html::Input>(cx);
+
     fn verify_value(value: &str) -> f32 {
-        value.parse::<f32>().expect("Invalid value")
+        value.parse::<f32>().unwrap_or(1.0)
     }
 
-    let update_rem_value = move |e: InputEvent| {
-        let res = verify_value(&event_target_value(&e)) * 16.0;
+    let update_rem_value = move |e: Event| {
+        let input_value = event_target_value(&e);
+        let res = verify_value(&input_value) * 16.0;
         set_px_value(res);
+        if let Some(input) = px_input_ref.get() {
+            input.set_value(&px_value().to_string());
+        }
     };
 
-    let handle_px_input = move |e: InputEvent| {
-        let res = verify_value(&event_target_value(&e));
+    let handle_px_input = move |e: Event| {
+        let input_value = event_target_value(&e);
+        let res = verify_value(&input_value);
         set_px_value(res);
+        if let Some(input) = rem_input_ref.get() {
+            input.set_value(&rem_value().to_string());
+        }
     };
 
     view! { cx,
-        <CalculatedInput on:input=update_rem_value calculated_value=rem_value  set_value=Signal::derive(cx, set_rem_value(px_value)) label="Rem" input_type=InputType::Rem />
-        <CalculatedInput calculated_value=px_value set_value=set_px_value label="Px" input_type=InputType::Px />
+        <CalculatedInput
+            on:input=update_rem_value
+            input_ref=rem_input_ref
+            calculated_value=Signal::derive(cx, rem_value)
+            label="Rem"  />
+        <CalculatedInput
+            on:input=handle_px_input
+            input_ref=px_input_ref
+            calculated_value=px_value
+            label="Px"
+        />
     }
 }
 
 #[component]
-fn CalculatedInput<F>(
+fn CalculatedInput(
     cx: Scope,
     #[prop(into)] calculated_value: Signal<f32>,
     label: &'static str,
-    on_value_changed: F,
-) -> impl IntoView
-where
-    F: Fn(InputEvent) + 'static,
-{
+    input_ref: NodeRef<Input>,
+) -> impl IntoView {
     view! {cx,
         <label>{label}</label>
-
         <input type="number"
-        on:input=on_value_changed
+        node_ref=input_ref
         placeholder={label} value={calculated_value} props:value={calculated_value} />
     }
 }
